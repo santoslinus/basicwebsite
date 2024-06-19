e6b6dd30c3a37e347381a4e5733638350a6811c4
santoslinus <linus@techdevs.org>
2024-06-19 19:03:35 +0000

    Added PHP

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+$name = $password = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    $vname = 'Mike';
+    $vpassword = 'Eagles';
+    if(preg_match("/[a-zA-Z-' ]*$/", $name))
+    {
+        $name = $_POST['name'];
+    }
+    if(preg_match("/[a-zA-Z0-9]*$/", $password))
+    {
+        $password = $_POST['password'];
+    }
+    if($name == $vname && $password == $vpassword)
+    {
+        header('Location: success.php');
+        exit;
+    }
+}
+?>
