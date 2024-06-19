3927e00d96fde4640d784c3c592216db1a65e0b5
santoslinus <linus@techdevs.org>
2024-06-19 13:08:11 -0700

    Changed login.php

--- login.php
+++ login.php
@@ -5,19 +5,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
     $vname = 'Mike';
     $vpassword = 'Eagles';
-    if(preg_match("/[a-zA-Z-' ]*$/", $name))
-    {
-        $name = $_POST['name'];
-    }
-    if(preg_match("/[a-zA-Z0-9]*$/", $password))
-    {
-        $password = $_POST['password'];
-    }
     if($name == $vname && $password == $vpassword)
     {
         $_SESSION['name'] = $name;
         header('Location: success.php');
         exit;
     }
+    else
+        echo "Invalid input.";
 }
 ?>
