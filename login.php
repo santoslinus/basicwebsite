b84490a3bfc80eac6f6eb79256123fb4ee67ac15
santoslinus <linus@techdevs.org>
2024-06-19 13:33:55 -0700

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php
 session_start();
 $name = $password = "";
+$vname = 'Mike';
+$vpassword = 'Eagles';
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    $vname = 'Mike';
-    $vpassword = 'Eagles';
+    $name = $_POST['name'];
+    $password = $_POST['password'];
     if($name == $vname && $password == $vpassword)
     {
         $_SESSION['name'] = $name;
